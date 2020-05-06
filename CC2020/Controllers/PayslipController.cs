97747147e8f992e932640bf9b0af37b4ef8aedfb
Andy Ellis <andy_ellis@mac.com>
2020-05-06 22:31:35 +1000

    Added controllers to basic model functionality (CRUD) -- added bare minimum views for controllers

--- CC2020/Controllers/PayslipController.cs
+++ CC2020/Controllers/PayslipController.cs
@@ -6,42 +6,54 @@ using CC2020.Data;
 using CC2020.Models;
 using CC2020.Services;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Logging;
 
 namespace CC2020.Controllers
 {
     public class PayslipController : Controller
     {
         private readonly IUnitOfWork unitOfWork;
+
         private readonly BusinessFacade businessRules;
 
-        public PayslipController(ApplicationDbContext context)
+        private readonly ILogger _logger;
+
+        public PayslipController(ApplicationDbContext context, ILogger<PayslipController> logger)
         {
             unitOfWork = new UnitOfWork(context);
             businessRules = new BusinessFacade();
+
+            _logger = logger;
         }
 
         public IActionResult Index()
         {
+            try
+            {
+                var empID = 4444;
+                var companyID = 40907025013;
+                var startDate = new DateTime(2020, 2, 17);
+                var endDate = startDate.AddDays(8);
 
-            var empID = 4444;
-            var companyID = 40907025013;
-            var startDate = new DateTime(2020, 2, 17);
-            var endDate = startDate.AddDays(8);
+                var payagreement = unitOfWork.PayAgreements.SingleOrDefault(
+                    x => x.EmployeeID == empID && x.CompanyID == companyID);
 
-            var payagreement = unitOfWork.PayAgreements.SingleOrDefault(
-                x => x.EmployeeID == empID && x.CompanyID == companyID);
+                var employee = unitOfWork.Employees.SingleOrDefault(x => x.EmployeeID == empID);
+                var company = unitOfWork.Companies.SingleOrDefault(x => x.ABN == companyID);
 
-            var employee = unitOfWork.Employees.SingleOrDefault(x => x.EmployeeID == empID);
-            var company = unitOfWork.Companies.SingleOrDefault(x => x.ABN == companyID);
-
-            var timesheets = unitOfWork.Timesheets.Find(
-                x => x.EmployeeID == empID && x.CompanyID == companyID
-                ).Where(x => x.Date.Date >= startDate && x.Date.Date < endDate);
+                var timesheets = unitOfWork.Timesheets.Find(
+                    x => x.EmployeeID == empID && x.CompanyID == companyID
+                    ).Where(x => x.Date.Date >= startDate && x.Date.Date < endDate);
 
 
-            var foo = businessRules.EmployeeWeeklyTimeSheet(employee, company, timesheets.ToList(), payagreement, 999);
+                var foo = businessRules.EmployeeWeeklyTimeSheet(employee, company, timesheets.ToList(), payagreement, 999);
 
+            }
+
+            catch(Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
             return View();
         }
     }
