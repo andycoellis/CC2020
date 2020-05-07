b6e7e0c777a4234eec359aa4ad51cbcc2fdf45b3
Andy Ellis <andy_ellis@mac.com>
2020-05-07 14:48:58 +1000

    added quick testing api

---
+++ CC2020/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CC2020.Data;
+using CC2020.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC2020.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public TestController(ApplicationDbContext context)
+        {
+            unitOfWork = new UnitOfWork(context);
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public IEnumerable<Employee> GetAll()
+        {
+            return unitOfWork.Employees.GetAll();
+        }
+    }
+}
